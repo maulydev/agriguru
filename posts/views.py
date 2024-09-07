c1fdf16c6c438db582319666fd53a9494f5c3101
Ernest Kumashie <mauly.dev@gmail.com>
2024-09-07 16:17:31 +0000

    Updated Post model and serializers

--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,7 @@ from .serializers import PostReadSerializer, PostCreateSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    filterset_fields = ['produce', 'farmer', 'is_negotiable', 'is_sold_out']
+    filterset_fields = ['farmer__user__id', 'farmer', 'is_negotiable', 'is_sold_out']
     
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
