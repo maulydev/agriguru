91cd83174de50427387cde95c7539b8d6e31614d
Ernest Kumashie <mauly.dev@gmail.com>
2024-09-10 19:14:57 +0000

    Add rejected field to PurchaseResponse model

--- agriguru/settings.py
+++ agriguru/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +53,9 @@ INSTALLED_APPS = [
     'payment.apps.PaymentConfig',
     'django_filters',
 ]
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
+SILENCED_SYSTEM_CHECKS = ["security.W019"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,25 @@
+# from rest_framework import viewsets
+# from .models import Post
+# from .serializers import PostReadSerializer, PostCreateSerializer
+
+# class PostViewSet(viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     filterset_fields = ['farmer__user__id', 'farmer', 'is_negotiable', 'is_sold_out']
+    
+#     def get_serializer_class(self):
+#         if self.action in ['create', 'update', 'partial_update']:
+#             return PostCreateSerializer
+#         return PostReadSerializer
+
 from rest_framework import viewsets
+from rest_framework.parsers import MultiPartParser, FormParser
 from .models import Post
 from .serializers import PostReadSerializer, PostCreateSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     filterset_fields = ['farmer__user__id', 'farmer', 'is_negotiable', 'is_sold_out']
+    parser_classes = (MultiPartParser, FormParser)
     
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
