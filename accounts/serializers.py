4b63303c89e8a70d84cf51017ac59631cf64b9c1
Ernest Kumashie <mauly.dev@gmail.com>
2024-09-19 09:04:59 +0000

    update

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -64,7 +64,35 @@ class OTPVerifySerializer(serializers.Serializer):
         return user
 
 
+# class ProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Profile
+#         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']  # Add other fields you want to update
+
+
+
 class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Profile
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        # Update User fields
+        user_data = validated_data.pop('user', {})
+        for attr, value in user_data.items():
+            setattr(instance.user, attr, value)
+        instance.user.save()
+
+        # Update Profile fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        
+        return instance
